7b616b2e17fceb897302be59381cb9252c3770cc
Dorota Miler <dorota.miler@capgemini.com>
2025-05-06 17:21:51 +0200

    Initialize Spring Boot project for doctor appointment booking

---
+++ src/test/java/com/dorotamiler/doctor_appointment_booking_app/DoctorAppointmentBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dorotamiler.doctor_appointment_booking_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAppointmentBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
